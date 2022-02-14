d9dc5cd850249f53e66ab4fe5aca758ccc0b4374
Julia Fisher <juliafisher@Julias-MacBook-Pro.local>
2022-02-14 15:02:34 -0500

    My first Bank Repository

---
+++ src/com/company/BankFranchise4.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class BankFranchise4 {
+
+    public static void main(String[] args) {
+	cashInVault = 100,000;
+
+        // "Set" accessor method / "setter":
+        public void setYearBuilt(double) { // "cash" is the "parameter" of the "setYearBuilt" method.
+            yearBuilt = year;
+        }
+
+        // "Get" accessor method / "getter":
+        public int getYearBuilt() {
+            return yearBuilt;
+    }
+}
